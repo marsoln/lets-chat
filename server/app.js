293f26ec10bbbdd3fc9259670f39648db5a75ba2
Marsoln <marsoln1988@gmail.com>
2015-12-10 15:06:48 +0800

    change

--- server/app.js
+++ server/app.js
@@ -1,37 +1,65 @@
 var express = require('express')
   , app = express()
   , mongoose = require('mongoose')
-  , jsonResHeader = (res)=> {
-    res[set].call(null, {
-      'Content-Type': 'text/json'
-      , 'Encoding': 'utf8'
-    });
-  }
   ;
 
 mongoose.connect('mongodb://localhost/test');
 
 var User = mongoose.model('Users', {
   name: String
-  , friends: [String]
   , age: Number
 });
 
-var xiaoming = new User({
-  name: 'Xiao ming',
-  friends: ['Xiao qiang', 'Meng xue'],
-  age: 18
-});
+var createUser=(name,age)=>{
+  var _user = new User({
+    name: name,
+    age: age
+  });
 
-xiaoming.save((err)=> {
-  console.log(err);
-});
+  _user.save((err)=> {
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(`${_user.name} Saved...`);
+    }
+  });
+};
+
+// createUser('xiaoming',24);
+// createUser('xiaoqiang',25);
 
 app
   .get('/', (req, res) => {
 
   })
   .get('/data', (req, res) => {
-    jsonResHeader(res);
+  'use strict'
+    res.set({
+      'Content-Type': 'text/json'
+      , 'Encoding': 'utf8'
+    });
+
+    User
+      .find({
+        name:req.query.name
+      })
+      .select('name age friends')
+      // .limit(1)
+      // .select({friends:1})
+      .exec((err,users)=>{
+        console.log(users);
+        if(!err){
+          res.send(users)
+        }
+        else {
+          res.send('error!')
+        }
+      })
+      ;
+
+  })
+  .listen(9000,(req,res)=>{
+    console.log('Server is running at port 9000.');
   })
 ;
