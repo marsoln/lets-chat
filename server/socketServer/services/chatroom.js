03861ba411bfcb78535a1a73b75578c2e7b73c68
marsoln <marsoln1988@gmail.com>
2016-08-29 19:11:55 +0800

    支持手机端的聊天室

--- server/socketServer/services/chatroom.js
+++ server/socketServer/services/chatroom.js
@@ -1,19 +1,20 @@
 'use strict'
 let numUsers = 0
+const SERVICE_NAME_PREFIX = 'chatroom-'
 
 module.exports = function (socket) {
   var __added = false
-  socket.on('new message', function (data) {
+  socket.on(`${SERVICE_NAME_PREFIX}new message`, function (data) {
     var msgData = {
       sender: socket.user,
       createTime: new Date(),
       message: data.message
     }
-    socket.broadcast.emit('new message', msgData)
+    socket.broadcast.emit(`${SERVICE_NAME_PREFIX}new message`, msgData)
   })
 
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (userInfo) {
+  socket.on(`${SERVICE_NAME_PREFIX}add user`, function (userInfo) {
     if (__added) return
 
     // we store the username in the socket session for this client
@@ -21,39 +22,39 @@ module.exports = function (socket) {
     ++numUsers
     __added = true
 
-    socket.emit('login', {
+    socket.emit(`${SERVICE_NAME_PREFIX}login`, {
       numUsers: numUsers
     })
 
     // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
+    socket.broadcast.emit(`${SERVICE_NAME_PREFIX}user joined`, {
       user: socket.user,
       numUsers: numUsers
     })
   })
 
   // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
+  socket.on(`${SERVICE_NAME_PREFIX}typing`, function () {
+    socket.broadcast.emit(`${SERVICE_NAME_PREFIX}typing`, {
       user: socket.user,
       _id: socket.id
     })
   })
 
   // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
+  socket.on(`${SERVICE_NAME_PREFIX}stop typing`, function () {
+    socket.broadcast.emit(`${SERVICE_NAME_PREFIX}stop typing`, {
       _id: socket.id
     })
   })
 
   // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
+  socket.on(`${SERVICE_NAME_PREFIX}disconnect`, function () {
     if (__added) {
       --numUsers
 
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
+      socket.broadcast.emit(`${SERVICE_NAME_PREFIX}user left`, {
         user: socket.user,
         numUsers: numUsers
       })
